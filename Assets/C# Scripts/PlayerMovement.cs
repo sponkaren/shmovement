5eaebcf1de5754fea6b1bed198f1fefc75eb1e3b
Gustaf Engsner <gustaf.engsner@gmail.com>
2023-09-01 13:08:36 +0200

    updated movement

--- Assets/C# Scripts/PlayerMovement.cs
+++ Assets/C# Scripts/PlayerMovement.cs
@@ -5,21 +5,30 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private BoxCollider2D col;
     private Animator anim;
     private SpriteRenderer sprite;
+
+    [SerializeField] private LayerMask jumpableGround;
 
     private float dirX = 0f;
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private float jumpForce = 14f;
+    [SerializeField] private float doubleJumpForce = 14f;
+    [SerializeField] private int doubleJumpsCount = 1;
+    [SerializeField] private int doubleJumpsLeft;
 
-    private enum MovemetState {idle,running,jumping,falling}
-    private MovemetState state;
+    private enum MovementState {idle,running,jumping,falling}
+    private MovementState state;
+    private MovementState previousState;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        col = GetComponent<BoxCollider2D>();
         anim = GetComponent<Animator>();
         sprite = GetComponent<SpriteRenderer>();
+        doubleJumpsLeft = doubleJumpsCount;
     }
 
     void Update()
@@ -28,43 +37,71 @@ public class PlayerMovement : MonoBehaviour
 
         rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
 
-        if (Input.GetButtonDown ("Jump") && (int)state<(int)MovemetState.jumping) 
+        if (Input.GetButtonDown ("Jump")) 
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            if(IsGrounded())
+            {
+                Debug.Log("Grounded jump");
+                rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            }
+            else if(doubleJumpsLeft > 0)
+            {
+                Debug.Log("Double Jumping!");
+                rb.velocity = new Vector2(rb.velocity.x, doubleJumpForce);
+                doubleJumpsLeft--;
+            }        
         }
-
         UpdateAnimationState();
 
     }
 
     private void UpdateAnimationState()
-    {              
+    {   
+        previousState = state;
+
         if (dirX > 0f)
         {
-            state = MovemetState.running;
+            state = MovementState.running;
             sprite.flipX = false;
         }
         else if (dirX < 0f)
         {
-            state = MovemetState.running;
+            state = MovementState.running;
             sprite.flipX = true;
         }
         else
         {
-            state = MovemetState.idle;
+            state = MovementState.idle;
         }
 
         
         if(rb.velocity.y > .01f)
         {
-            state = MovemetState.jumping;
+            state = MovementState.jumping;
         }
         else if(rb.velocity.y < -.01f) 
         {
-            state = MovemetState.falling;
+            state = MovementState.falling;
         }
         
-
         anim.SetInteger("state", (int)state);
+
+        if((int)previousState > (int)MovementState.running && (int)state < (int)MovementState.jumping)
+        {
+            if(IsGrounded())
+            {
+                resetDoubleJumps();
+            }
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.BoxCast(col.bounds.center, col.bounds.size- new Vector3(.1f,.1f,.1f), 0f, Vector2.down, .2f, jumpableGround);
+    }
+
+    private void resetDoubleJumps()
+    {
+        doubleJumpsLeft = doubleJumpsCount;
     }
 }
